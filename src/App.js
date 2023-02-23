64e7dba74d5bdfa31b371a4cde21e3a240b7b83d
BawerMY <bawermy123@gmail.com>
2023-02-23 22:46:44 +0100

    search logic base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Country from './components/Country';
+var $ = require('jquery-browserify')
 
 function App() {
+  var dark = false
+  const [page, setPage] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen min-w-screen">
+      <Search />
+      {page}
     </div>
   );
+
+
+
+  function Search() {
+    const [ress, setRess] = useState(null)
+    function search(input) {
+      input = input.replace(' ','')
+      if(input==='') {setRess(null); return}
+        var results = []
+        var all
+        $.ajax({
+            dataType: 'json',
+            url: 'https://restcountries.com/v3.1/all/',
+            async: false,
+            success: function(data) {all = data}
+        })
+        for(var i = 0; i <all.length; i++) {
+            if(all[i].name.common.toLowerCase().includes(input.toLowerCase())) results.push(all[i].name.common.toLowerCase())
+            if(all[i].region.toLowerCase().includes(input.toLowerCase())) results.push(all[i].name.common.toLowerCase())
+            // if(all[i].name.official.toLowerCase().includes(input.toLowerCase())) results.push()
+            // if(all[i].capital.toLowerCase().includes(input.toLowerCase())) results.push(<a>Capital: {all[i].capital} id:{i}</a>)
+        }
+        var results2 = []
+        for(var i = 0; i <results.length; i++) {
+          if(!results2.includes(results[i])) results2.push(results[i])
+        }
+        setRess(results2.map((result)=><div className='w-[10rem] p-1 border-2 border-black m-1' key={result} onClick={() => setPage(<Country name={result} />)}>{result}</div>))
+    }
+    return (
+    <div className='w-[20rem] max-[460]:w-screen flex max-[460]:items-center flex-col'>
+      <input onChange={(e) => {search(e.target.value)}} placeholder='Search...' className='w-screen max-w-[30rem] px-5 border-2 border-black h-10 rounded-lg' type="text" />
+      <div className='flex flex-wrap w-screen max-w-[30rem] border-black border-b-[1px]'>
+        {ress}
+      </div>
+    </div>
+    )
+  }
+  
+  
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
---
+++ src/components/Country.jsx
@@ -0,0 +1,29 @@
+var $ = require('jquery-browserify')
+export default function Country(props) {
+    var country
+    $.ajax({
+        dataType: 'json',
+        url: 'https://restcountries.com/v3.1/name/'+props.name,
+        async: false,
+        success: function(data) {country = data[0]}
+    })
+    return (
+        
+    <div className='flex flex-col'>
+        <div className='flex flex-wrap'>
+            <img className='w-[30vw]' src={country.flags.svg} alt="" />
+            <span className='text-[30px] px-5 p-2 title'>{country.name.common}</span>
+        </div>
+        <div className='flex flex-col p-5 gap-2.5'>
+            {country.name.common!==country.name.official&&<span className=''>Official name: "{country.name.official}"</span>}
+            <div className='bg-[#5aff44b6] p-5 max-w-[20rem] rounded-xl'>Continent: {country.continents}
+                <div className='bg-[#08ceff] p-5 rounded-xl'>{country.name.common}
+                    <div className='bg-[#ff5252] p-5 rounded-xl'>Capital: {country.capital}</div>
+                </div>
+            </div>
+            <span>Currency: {country.currencies[Object.keys(country.currencies)[0]].name}({country.currencies[Object.keys(country.currencies)[0]].symbol})</span>
+            <span>Timezone(s): {country.timezones.map((timezone)=><>{timezone} </>)}</span>
+        </div>
+    </div>
+    )
+}
